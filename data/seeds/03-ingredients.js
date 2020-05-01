d60ffbab0655c7c239b70fa9edb5bf46564ab4b3
Harry Merritt <harry@merritt.es>
2020-05-01 02:50:30 +0100

    Create all seeds

--- data/seeds/03-ingredients.js
+++ data/seeds/03-ingredients.js
@@ -1,13 +1,11 @@
-
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+exports.seed = function (knex) {
+    return knex("ingredients").insert([
+        { id: 1, name: "flower" },
+        { id: 2, name: "butter" },
+        { id: 3, name: "lasagna" },
+        { id: 4, name: "beef" },
+        { id: 5, name: "oats" },
+        { id: 6, name: "cheese" },
+        { id: 7, name: "sugar" },
+    ]);
 };
--- data/seeds/04-recipes-ingredients.js
+++ data/seeds/04-recipes-ingredients.js
@@ -1,13 +1,15 @@
+exports.seed = function (knex) {
+    return knex("recipes_ingredients").insert([
+        { recipe_id: 1, ingredient_id: 1, quantity: 12 },
+        { recipe_id: 1, ingredient_id: 2, quantity: 4 },
+        { recipe_id: 1, ingredient_id: 7, quantity: 2 },
 
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+        { recipe_id: 2, ingredient_id: 5, quantity: 55 },
+        { recipe_id: 2, ingredient_id: 2, quantity: 8 },
+        { recipe_id: 2, ingredient_id: 7, quantity: 3 },
+
+        { recipe_id: 3, ingredient_id: 4, quantity: 42 },
+        { recipe_id: 3, ingredient_id: 3, quantity: 23 },
+        { recipe_id: 3, ingredient_id: 6, quantity: 6 },
+    ]);
 };
