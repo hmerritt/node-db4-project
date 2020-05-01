a8e5ccb1d12402a5a14214630c204e6294b111f1
Harry Merritt <harry@merritt.es>
2020-05-01 03:05:23 +0100

    Self practice SQL queries

---
+++ queries.sql
@@ -0,0 +1,17 @@
+-- Self practice SQL queries
+
+-- Gets recipe and instruction
+SELECT [recipes].name, [steps].instruction
+FROM [recipes]
+JOIN [steps]
+ON [steps].recipe_id == [recipes].Id
+ORDER BY [recipes].name AND [steps]."order" ASC;
+
+-- Get recipe name, ingredient name and quantity
+SELECT [recipes].name, [ingredients].name as "ingredient", [recipes_ingredients].quantity
+FROM [ingredients]
+JOIN [recipes_ingredients]
+ON [ingredients].id == [recipes_ingredients].ingredient_id
+JOIN [recipes]
+ON [recipes].id == [recipes_ingredients].recipe_id
+ORDER BY [ingredients].name ASC;
