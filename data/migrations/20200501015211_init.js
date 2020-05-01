571a9c7525fe1800d89fe8a6db7db756a4511a16
Harry Merritt <harry@merritt.es>
2020-05-01 02:50:20 +0100

    Add CASCADE option for all references

--- data/migrations/20200501015211_init.js
+++ data/migrations/20200501015211_init.js
@@ -1,35 +1,45 @@
-
-exports.up = function(knex) {
-    return (
-        knex.schema
-            .createTable("recipes", table => {
-                table.increments();
-                table.text("name").unique().notNullable();
-            })
-            .createTable("steps", table => {
-                table.increments();
-                table.integer("recipe_id").unsigned().notNullable()
-                    .references("id")
-                    .inTable("recipes");
-                table.text("instruction").notNullable();
-            })
-            .createTable("ingredients", table => {
-                table.increments();
-                table.text("name").unique().notNullable();
-            })
-            .createTable("recipes_ingredients", table => {
-                table.integer("recipe_id").unsigned().notNullable()
-                    .references("id")
-                    .inTable("recipes");
-                table.integer("ingredients").unsigned().notNullable()
-                    .references("id")
-                    .inTable("recipes");
-                table.float("quantity").notNullable();
-            })
-    )
+exports.up = function (knex) {
+    return knex.schema
+        .createTable("recipes", (table) => {
+            table.increments();
+            table.text("name").unique().notNullable();
+        })
+        .createTable("steps", (table) => {
+            table.increments();
+            table
+                .integer("recipe_id")
+                .unsigned()
+                .notNullable()
+                .references("recipes.id")
+                .onDelete("CASCADE")
+                .onUpdate("CASCADE");
+            table.integer("order").notNullable();
+            table.text("instruction").notNullable();
+        })
+        .createTable("ingredients", (table) => {
+            table.increments();
+            table.text("name").unique().notNullable();
+        })
+        .createTable("recipes_ingredients", (table) => {
+            table
+                .integer("recipe_id")
+                .unsigned()
+                .notNullable()
+                .references("recipes.id")
+                .onDelete("CASCADE")
+                .onUpdate("CASCADE");
+            table
+                .integer("ingredient_id")
+                .unsigned()
+                .notNullable()
+                .references("ingredients.id")
+                .onDelete("CASCADE")
+                .onUpdate("CASCADE");
+            table.float("quantity").notNullable();
+        });
 };
 
-exports.down = function(knex) {
+exports.down = function (knex) {
     return knex.schema
         .dropTableIfExists("recipes")
         .dropTableIfExists("steps")
